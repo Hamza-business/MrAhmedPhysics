10c9eb6b7147fe03d756d04d6a08a78872c02d06
Hamza Elkotb <97664403+Hamza-business@users.noreply.github.com>
2023-06-21 17:53:44 +0300

    books

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,129 +41,129 @@ showPops.forEach((ele) => {
 
 
 
-const valPhInput = document.querySelector("[valPh='input']");
-const valPhbox = document.querySelector("[valPh='box']");
-const valPhBtn = document.querySelector("[valPh='codeAsk']");
-const valPhcodePlc = document.querySelector("[valPh='codePlace']");
-const codeSubmit = document.querySelector('[valPh="codeSubmit"]');
+// const valPhInput = document.querySelector("[valPh='input']");
+// const valPhbox = document.querySelector("[valPh='box']");
+// const valPhBtn = document.querySelector("[valPh='codeAsk']");
+// const valPhcodePlc = document.querySelector("[valPh='codePlace']");
+// const codeSubmit = document.querySelector('[valPh="codeSubmit"]');
 
-const resentTimer = function(ele){
-    let countdown = 60;  
-    function updateTimer() {
-        ele.textContent = `أعادة الارسال ${countdown}`;
-        countdown--;
+// const resentTimer = function(ele){
+//     let countdown = 60;  
+//     function updateTimer() {
+//         ele.textContent = `أعادة الارسال ${countdown}`;
+//         countdown--;
   
-        if (countdown >= 0) {
-            setTimeout(updateTimer, 900);
-        } else {
-            ele.removeAttribute('disabled');
-            ele.style.opacity = 1;
-            ele.textContent = `أعادة الارسال`;
-        }
-    }  
-    setTimeout(updateTimer, 0);
-}
-const showValidationBox = function(){
-    valPhbox.style.display = "flex";
-    setTimeout(() => {
-        valPhbox.classList.add("active")
-    }, 0);
-}
-const varificationButton = function(){
-    valPhBtn?.addEventListener("click", (e)=>{
-        e.preventDefault()
-        valPhBtn.style.opacity = 0.3;
-        if(valPhBtn.getAttribute("disabled") != "true"){
-            valPhBtn.textContent = "اعادة الارسال 60";
-            resentTimer(valPhBtn)
-        }
-        valPhBtn.setAttribute("disabled","true");
-        valPhInput.setAttribute("disabled","true");
+//         if (countdown >= 0) {
+//             setTimeout(updateTimer, 900);
+//         } else {
+//             ele.removeAttribute('disabled');
+//             ele.style.opacity = 1;
+//             ele.textContent = `أعادة الارسال`;
+//         }
+//     }  
+//     setTimeout(updateTimer, 0);
+// }
+// const showValidationBox = function(){
+//     valPhbox.style.display = "flex";
+//     setTimeout(() => {
+//         valPhbox.classList.add("active")
+//     }, 0);
+// }
+// const varificationButton = function(){
+//     valPhBtn?.addEventListener("click", (e)=>{
+//         e.preventDefault()
+//         valPhBtn.style.opacity = 0.3;
+//         if(valPhBtn.getAttribute("disabled") != "true"){
+//             valPhBtn.textContent = "اعادة الارسال 60";
+//             resentTimer(valPhBtn)
+//         }
+//         valPhBtn.setAttribute("disabled","true");
+//         valPhInput.setAttribute("disabled","true");
 
-        valPhcodePlc.style.display = "block";
-        setTimeout(() => {
-            valPhcodePlc.style.opacity = 1;
-        }, 0);
-    });
-}
-varificationButton()
+//         valPhcodePlc.style.display = "block";
+//         setTimeout(() => {
+//             valPhcodePlc.style.opacity = 1;
+//         }, 0);
+//     });
+// }
+// varificationButton()
 
-const checkCodeLen = function(){
-    valPhcodePlc?.addEventListener('input', ()=>{
-        if(valPhcodePlc.value.length == 6){
-            codeSubmit.style.display = "block";
-            setTimeout(() => {
-                codeSubmit.classList.add("active");
-            }, 0);
-        }
-        else{
-            codeSubmit.classList.remove("active");
-            setTimeout(() => {
-                codeSubmit.style.display = "none";
-            }, 0);
-        }
-    })
-}
-checkCodeLen()
-
-
+// const checkCodeLen = function(){
+//     valPhcodePlc?.addEventListener('input', ()=>{
+//         if(valPhcodePlc.value.length == 6){
+//             codeSubmit.style.display = "block";
+//             setTimeout(() => {
+//                 codeSubmit.classList.add("active");
+//             }, 0);
+//         }
+//         else{
+//             codeSubmit.classList.remove("active");
+//             setTimeout(() => {
+//                 codeSubmit.style.display = "none";
+//             }, 0);
+//         }
+//     })
+// }
+// checkCodeLen()
 
 
 
 
-const phoneInputs = document.querySelectorAll("[type='tel']");
-let chackState = [false, false, false];
-function studentPhoneCheck(phone) {
-    if (phone.value[0] != "0") {
-        phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يبدأ الرقم بصفر"
-        phone.style.borderColor = 'red'
-        chackState[0] = false;
-    } else {
-        if (phone.value.length != 11) {
-            phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يتكون الرقم من 11 رقم"
-            phone.style.borderColor = 'red'
-            chackState[0] = false;
-        } else {
-            phone.closest("[phonNumBox]").querySelector(".help").textContent = ""
-            phone.style.borderColor = ''
-            chackState[0] = true;
-            showValidationBox()
-        }
-    }
-}
-function phoneCheck(phone, second, i) {
-    if (phone.value[0] != "0") {
-        phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يبدأ الرقم بصفر"
-        phone.style.borderColor = 'red'
-        chackState[i] = false;
-    } else {
-        if (phone.value.length != 11) {
-            phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يتكون الرقم من 11 رقم"
-            phone.style.borderColor = 'red'
-            chackState[i] = false;
-        } else {
-            if(phone.value == second.value){
-                phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يختلف رقم ولي الامر الاول عن الثاني"
-                phone.style.borderColor = 'red'
-                chackState[i] = false;
-            }else{
-                phone.closest("[phonNumBox]").querySelector(".help").textContent = ""
-                phone.style.borderColor = ''
-                chackState[i] = true;
-            }
-        }
-    }
-}
 
-phoneInputs[0]?.addEventListener('input', ()=>{
-    studentPhoneCheck(phoneInputs[0]);
-})
-phoneInputs[1]?.addEventListener('input', ()=>{
-    phoneCheck(phoneInputs[1], phoneInputs[2], 1);
-})
-phoneInputs[2]?.addEventListener('input', ()=>{
-    phoneCheck(phoneInputs[2], phoneInputs[1], 2);
-})
+
+// const phoneInputs = document.querySelectorAll("[type='tel']");
+// let chackState = [false, false, false];
+// function studentPhoneCheck(phone) {
+//     if (phone.value[0] != "0") {
+//         phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يبدأ الرقم بصفر"
+//         phone.style.borderColor = 'red'
+//         chackState[0] = false;
+//     } else {
+//         if (phone.value.length != 11) {
+//             phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يتكون الرقم من 11 رقم"
+//             phone.style.borderColor = 'red'
+//             chackState[0] = false;
+//         } else {
+//             phone.closest("[phonNumBox]").querySelector(".help").textContent = ""
+//             phone.style.borderColor = ''
+//             chackState[0] = true;
+//             showValidationBox()
+//         }
+//     }
+// }
+// function phoneCheck(phone, second, i) {
+//     if (phone.value[0] != "0") {
+//         phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يبدأ الرقم بصفر"
+//         phone.style.borderColor = 'red'
+//         chackState[i] = false;
+//     } else {
+//         if (phone.value.length != 11) {
+//             phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يتكون الرقم من 11 رقم"
+//             phone.style.borderColor = 'red'
+//             chackState[i] = false;
+//         } else {
+//             if(phone.value == second.value){
+//                 phone.closest("[phonNumBox]").querySelector(".help").textContent = "يجب أن يختلف رقم ولي الامر الاول عن الثاني"
+//                 phone.style.borderColor = 'red'
+//                 chackState[i] = false;
+//             }else{
+//                 phone.closest("[phonNumBox]").querySelector(".help").textContent = ""
+//                 phone.style.borderColor = ''
+//                 chackState[i] = true;
+//             }
+//         }
+//     }
+// }
+
+// phoneInputs[0]?.addEventListener('input', ()=>{
+//     studentPhoneCheck(phoneInputs[0]);
+// })
+// phoneInputs[1]?.addEventListener('input', ()=>{
+//     phoneCheck(phoneInputs[1], phoneInputs[2], 1);
+// })
+// phoneInputs[2]?.addEventListener('input', ()=>{
+//     phoneCheck(phoneInputs[2], phoneInputs[1], 2);
+// })
 
 
 
@@ -187,12 +187,12 @@ passwordInputs.forEach((passInp, i)=>{
 
 
 
-document.getElementById("submit")?.addEventListener("click", function (event) {
-    if (!(chackState[0] && chackState[1] && chackState[2] & passEqualtCheck)) {
-        event.preventDefault()
-        alert("يرجى ادخال البيانات المطلوبة بشكل صحيح")
-    }
-});
+// document.getElementById("submit")?.addEventListener("click", function (event) {
+//     if (!(chackState[0] && chackState[1] && chackState[2] & passEqualtCheck)) {
+//         event.preventDefault()
+//         alert("يرجى ادخال البيانات المطلوبة بشكل صحيح")
+//     }
+// });
 
 
 
@@ -290,7 +290,7 @@ function notificationPopShowHide(){
             document.querySelector('.trans').classList.remove("showNot");
     }, 12000);
 }
-notificationPopShowHide()
+// notificationPopShowHide()
 
 
 
@@ -413,11 +413,11 @@ stopButton?.addEventListener('click', (e)=>{
 
 
 
-tinymce?.init({
-    selector: '#markupTextarea',
-    plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount',
-    toolbar: 'undo redo | formatselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | removeformat | help'
-});
+// tinymce?.init({
+//     selector: '#markupTextarea',
+//     plugins: 'advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount',
+//     toolbar: 'undo redo | formatselect | bold italic underline strikethrough | forecolor backcolor | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | removeformat | help'
+// });
 
 
 
